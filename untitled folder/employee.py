76319974d329374fcc8df1303c73b56307167557
Gregory Adler <greg7239@gmail.com>
2017-02-14 22:44:56 -0500

    Python practice - Employee Problem

---
+++ untitled folder/employee.py
@@ -0,0 +1,15 @@
+def laidoff(n, k):
+	dictionary = {}
+	dictionary[str(1)] = 1
+	for i in range (2, n+1):
+		key = str(i -1)
+		total = dictionary[key] + k
+		remainder = (dictionary[(key)] + k) % i
+		if remainder == 0:
+			dictionary[str(i)] = n
+		else:
+			dictionary[str(i)] = remainder
+
+	print dictionary[str(n)]
+
+laidoff(6,3);
