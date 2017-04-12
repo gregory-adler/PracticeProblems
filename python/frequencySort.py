ad1430ccc2d3ad4742d8b11dd8865afbb8853b59
Gregory Adler <greg7239@gmail.com>
2017-04-11 23:20:52 -0400

    secondTree method

--- python/frequencySort.py
+++ python/frequencySort.py
@@ -5,22 +5,47 @@ class Node:
 		self.left = None
 		self.right = None
 
-def insertNode(root, ordinance):
-	currentNode = root
+def insertNode(currentNode, ordinance):
 
-	if (ordinance < root.value):
+	if (ordinance < currentNode.key):
 		if (currentNode.left != None):
-			currentNode = currentNode.left
+			insertNode(currentNode.left, ordinance)
 		else:
 			currentNode.left = Node(ordinance, 1)
-	elif (ordinance > root. value):
+	elif (ordinance > currentNode. key):
 		if (currentNode.right != None):
-			currentNode = currentNode.right
+			insertNode(currentNode.right)
 		else:
 			currentNode.right = Node(ordinance, 1)
 	# key = vvalue
 	else:
+		print "same condition"
 		currentNode.value += 1
+
+def createSecondTree(currentNode, secondTreeRoot):
+	if (currentNode.left != None):
+		createSecondTree(currentNode.left, secondTreeRoot)
+	elif (currentNode.right != None):
+		createSecondTree(currentNode.right, secondTreeRoot)
+	secondTree(secondTreeRoot, currentNode, currentNode.value)
+
+def secondTree(secondTreeRoot, currentNode, value):
+
+	# if value == root
+
+	if (secondTreeRoot == currentNode):
+		return
+	
+	if (value < currentNode.value):
+		if (currentNode.left != None):
+			currentNode = currentNode.left
+		else:
+			currentNode.left = Node(ordinance, 1)
+	elif (value > currentNode.value):
+		if (currentNode.right != None):
+			currentNode = currentNode.right
+		else:
+			currentNode.right = Node(ordinance, 1)
 
 
 def frequencySort(s):
@@ -29,6 +54,9 @@ def frequencySort(s):
 		ordinance = ord(s[i])
 		insertNode(root, ordinance)
 
+	secondTreeRoot = Node(root.value, root.key)
 
-s = "example"
+	createSecondTree(root, secondTreeRoot)
+
+s = "eeees"
 frequencySort(s)
