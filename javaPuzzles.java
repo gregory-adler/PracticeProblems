18aa194625449856c56d49d41820001ca7b25ec5
Gregory Adler <greg7239@gmail.com>
2019-02-10 14:22:17 -0800

    javaSetup

---
+++ javaPuzzles.java
@@ -0,0 +1,14 @@
+public class javaPuzzles{
+
+	public static void main(String args []){
+
+		System.out.println("Hello World");
+	}
+
+	public classPractice{
+		public classPractice(){
+			System.out.println("constructor");
+		}
+	}
+	
+}
